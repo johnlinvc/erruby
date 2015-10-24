dba0e04c6e0992e203d0fa72c3392beee07e4854
johnlinvc <johnlinvc@gmail.com>
2015-10-24 17:11:32 +0800

    seperate gen_server behavior from custom ones

--- src/erruby_object.erl
+++ src/erruby_object.erl
@@ -1,6 +1,7 @@
 -module(erruby_object).
 -behavior(gen_server).
--export([init/1, terminate/2, code_change/3, handle_call/3, handle_cast/2, handle_info/2, new_kernel/0, msg_send/3]).
+-export([init/1, terminate/2, code_change/3, handle_call/3, handle_cast/2, handle_info/2]).
+-export([new_kernel/0, msg_send/3]).
 
 init([]) ->
   Methods = #{puts => fun method_puts/1},
