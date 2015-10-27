838e383e3fa3066b297777c1aa7f252a3fd9d33e
johnlinvc <johnlinvc@gmail.com>
2015-10-27 17:21:51 +0800

    able to set/get local var

--- src/erruby_object.erl
+++ src/erruby_object.erl
@@ -1,11 +1,12 @@
 -module(erruby_object).
 -behavior(gen_server).
 -export([init/1, terminate/2, code_change/3, handle_call/3, handle_cast/2, handle_info/2]).
--export([new_kernel/0, msg_send/3]).
+-export([new_kernel/0, msg_send/3, lvasgn/3, lvar/2]).
 
 init([]) ->
   Methods = #{puts => fun method_puts/1},
-  State = #{self => self(), methods => Methods},
+  IVars = #{},
+  State = #{self => self(), methods => Methods, ivars => IVars},
   {ok, State}.
 
 start_link() ->
@@ -19,6 +20,12 @@ code_change(_OldVsn, State, _Extra) -> {ok, State}.
 msg_send(Self, Msg, Args) ->
   gen_server:call(Self, #{type => msg_send, msg => Msg, args => Args}).
 
+lvasgn(Self, Name, Val) ->
+  gen_server:call(Self, #{type => lvasgn, name => Name, val => Val}).
+
+lvar(Self, Name) ->
+  gen_server:call(Self, #{type => lvar, name => Name}).
+
 new_kernel() ->
   start_link().
 
@@ -31,6 +38,21 @@ handle_call(#{ type := msg_send, msg := Msg, args:= Args}=_Req, _From, State) ->
   Method(Args),
   NewState = State,
   {reply, done, NewState};
+
+handle_call(#{ type := lvasgn, name := Name, val:= Val }=_Req, _From, #{ivars := IVars}=State) ->
+  NewState = State#{ivars := IVars#{Name => Val}},
+  %TODO customer operation overloading
+  {reply, Val, NewState};
+
+handle_call(#{ type := lvar, name := Name}, _From, #{ivars := IVars}=State) ->
+  case maps:is_key(Name, IVars) of
+    true ->
+      #{Name := Val} = IVars,
+      {reply, {ok, Val}, State};
+    false ->
+      %TODO find with method
+      {reply, {error, 'undefined local variable'}, State}
+  end;
 
 handle_call(_Req, _From, State) ->
   io:format("handle unknow call ~p ~p ~p ~n",[_Req, _From, State]),
--- src/erruby_vm.erl
+++ src/erruby_vm.erl
@@ -2,10 +2,10 @@
 -export([eval_ast/1]).
 
 print_ast(Ast) ->
-  io:format("~p ~n",[Ast]).
+  io:format("Ast: ~p ~n",[Ast]).
 
 print_env(Env) ->
-  io:format("~p ~n",[Env]).
+  io:format("Env: ~p ~n",[Env]).
 
 eval_ast({ast,type,'begin',children, Children}, Env) ->
   io:format("eval begin~n",[]),
@@ -16,6 +16,16 @@ eval_ast({ast,type,send, children, Children}, Env) ->
   EvaledArgs = [ eval_ast(Child, Env) || Child <- Args],
   #{ klass := Self } = Env,
   erruby_object:msg_send(Self, Msg, EvaledArgs);
+
+eval_ast({ast, type, lvasgn, children, Children}, #{ klass := Self } = Env) ->
+  [Name, ValAst] = Children,
+  Val = eval_ast(ValAst, Env),
+  erruby_object:lvasgn(Self, Name, Val);
+
+eval_ast({ast, type, lvar, children, [Name]}, #{ klass := Self} = _Env) ->
+  {ok, Val} = erruby_object:lvar(Self, Name),
+  Val;
+
 
 eval_ast({ast, type, str, children, Children}, _Env) ->
   [SBin|_T] = Children,
