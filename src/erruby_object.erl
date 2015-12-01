6e5b09961a75421866937fa3d1c731bcfcb9b989
johnlinvc <johnlinvc@gmail.com>
2015-12-01 17:00:45 +0800

    add impl for single layer const

--- src/erruby_object.erl
+++ src/erruby_object.erl
@@ -1,7 +1,7 @@
 -module(erruby_object).
 -behavior(gen_server).
 -export([init/1, terminate/2, code_change/3, handle_call/3, handle_cast/2, handle_info/2]).
--export([new_kernel/0,  def_method/4, find_method/2]).
+-export([new_kernel/0,  def_method/4, find_method/2, def_const/3, find_const/2]).
 
 init([]) ->
   Methods = #{
@@ -9,7 +9,8 @@ init([]) ->
     self => fun method_self/1
    },
   IVars = #{},
-  State = #{self => self(), methods => Methods, ivars => IVars},
+  Consts = #{'Object' => self()},
+  State = #{self => self(), methods => Methods, ivars => IVars, consts => Consts},
   {ok, State}.
 
 start_link() ->
@@ -28,6 +29,13 @@ find_method(Self, Name) ->
 def_method(Self, Name, Args, Body) ->
   gen_server:call(Self, #{type => def_method, name => Name, args => Args, body => Body}).
 
+def_const(Self, Name, Value) ->
+  gen_server:call(Self, #{type => def_const, name => Name, value => Value}).
+
+find_const(Self, Name) ->
+  gen_server:call(Self, #{type => find_const, name => Name}).
+
+
 
 handle_info(Info, State) ->
   io:format("Got unkwon info:~n~p~n", [Info]),
@@ -41,6 +49,15 @@ handle_call(#{ type := def_method , name := Name, body := Body, args := Args}=_M
 handle_call(#{ type := find_method, name := Name }, _From, State) ->
   #{methods := #{Name := Method}} = State,
   {reply, Method, State};
+
+handle_call(#{ type := def_const, name := Name, value := Value }, _From, #{consts := Consts}=State) ->
+  NewConsts = Consts#{Name => Value},
+  NewState = State#{consts := NewConsts},
+  {reply, Name, NewState};
+
+handle_call(#{ type := find_const, name := Name }, _From, #{consts := Consts}=State) ->
+  #{Name := Value} = Consts,
+  {reply, Value, State};
 
 handle_call(_Req, _From, State) ->
   io:format("handle unknow call ~p ~p ~p ~n",[_Req, _From, State]),
