8199e7e0bb92604e75174d7bc8d73e224b60815a
johnlinvc <johnlinvc@gmail.com>
2015-12-05 12:16:03 +0800

    use correct class structure

--- src/erruby_object.erl
+++ src/erruby_object.erl
@@ -4,13 +4,18 @@
 -export([new_kernel/0,  def_method/4, find_method/2, def_const/3, find_const/2, init_object_class/0,object_class/0]).
 -export([init_class_class/0, class_class/0,new_class/0]).
 
-init([#{super_class := SuperClass}]) ->
-  default_init();
+init([#{class := Class}]) ->
+  DefaultState = default_state(),
+  {ok, add_class_to_state(DefaultState, Class)};
 
 init([]) ->
-  default_init().
+  {ok, default_state()}.
 
-default_init() ->
+add_class_to_state(State, Class) ->
+  State#{class => Class}.
+
+%TODO in method_class return defalut class_class if no class is present
+default_state() ->
   Methods = #{
     puts => fun method_puts/2,
     self => fun method_self/1,
@@ -18,24 +23,26 @@ default_init() ->
    },
   IVars = #{},
   Consts = #{'Object' => self()},
-  State = #{self => self(), methods => Methods, ivars => IVars, consts => Consts},
-  {ok, State}.
+  #{self => self(), methods => Methods, ivars => IVars, consts => Consts}.
 
 
 start_link() ->
   gen_server:start_link(?MODULE, [], []).
 
-start_link(SuperClass) ->
-  gen_server:start_link(?MODULE, [#{super_class => SuperClass }], []).
+start_link(Class) ->
+  gen_server:start_link(?MODULE, [#{class => Class }], []).
 
 terminate(_Arg, _State) ->
   {ok, dead}.
 
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
+get_class(Self) ->
+  gen_server:call(Self, #{type => get_class}).
 
 find_method(Self, Name) ->
-  gen_server:call(Self, #{type => find_method, name => Name}).
+  Klass = get_class(Self),
+  gen_server:call(Klass, #{type => find_method, name => Name}).
 
 %TODO: we need frame when running
 def_method(Self, Name, Args, Body) ->
@@ -71,6 +78,11 @@ handle_call(#{ type := find_const, name := Name }, _From, #{consts := Consts}=St
   Value = maps:get(Name, Consts, nil),
   {reply, Value, State};
 
+handle_call(#{ type := get_class}, _From, State) ->
+  Value = maps:get(class, State, class_class()),
+  {reply, Value, State};
+
+
 handle_call(_Req, _From, State) ->
   io:format("handle unknow call ~p ~p ~p ~n",[_Req, _From, State]),
   NewState = State,
@@ -90,8 +102,9 @@ method_self(#{self := Self}=Env) ->
   Env#{ret_val => Self}.
 
 %FIXME new a real class
-method_new(#{self := Self}=Env) ->
-  Env#{ret_val => Self}.
+method_new(#{self := Klass}=Env) ->
+  {ok, NewObject} = start_link(Klass),
+  Env#{ret_val => NewObject}.
 
 new_kernel() ->
   start_link().
@@ -110,5 +123,5 @@ object_class() ->
   whereis(erruby_object_class).
 
 class_class() ->
-  whereis(erruby_object_class).
+  whereis(erruby_class_class).
 
