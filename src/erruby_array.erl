95d150fa48081b04022f8b2e4e149aff13104c18
johnlinvc <johnlinvc@gmail.com>
2015-12-13 17:25:05 +0900

    add erruby_array:at

--- src/erruby_array.erl
+++ src/erruby_array.erl
@@ -1,6 +1,7 @@
 -module(erruby_array).
 -export([install_array_classes/0, new_array/2]).
 
+%TODO find a way to define module_function
 install_array_classes() ->
   {ok, ArrayClass} = erruby_class:new_class(),
   'Array' = erruby_object:def_global_const('Array', ArrayClass),
@@ -12,3 +13,10 @@ new_array(Env, Elements) ->
   Properties = #{elements => Elements},
   {ok, Array} = erruby_object:new_object(ArrayClass, Properties),
   Env#{ret_val => Array}.
+
+%% @doc the Index is 0-based, not the 1-based of usual erlang
+at(Array, Index) ->
+  Properties = erruby_object:get_properties(Array),
+  #{ elements := Elements} = Properties,
+  list:nth(Index+1, Elements).
+
