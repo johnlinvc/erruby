cd15e09ada5c3fe1eb27562842a4141824ec0fed
johnlinvc <johnlinvc@gmail.com>
2016-01-10 20:21:40 +0800

    add ret_val function to erruby_rb

--- src/erruby_rb.erl
+++ src/erruby_rb.erl
@@ -1,6 +1,8 @@
 %%% TODO move general runtime api to here
 -module(erruby_rb).
--export([ret_self/1]).
+-export([ret_self/1,ret_val/1]).
 
 ret_self( #{self := Self} = Env ) ->
   Env#{ret_val => Self}.
+
+ret_val( #{ret_val := RetVal} ) -> RetVal.
