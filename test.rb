6732e593382ec7954439e3677e5f91d75a1c9170
johnlinvc <johnlinvc@gmail.com>
2016-05-19 18:23:48 +0800

    add verbose to test script

--- test.rb
+++ test.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby
 fail_case = []
+verbose = ARGV[0] == "-v"
 Dir.glob("rb_test/*.rb") do |fn|
   basename = File.basename(fn,'.rb')
   next if basename.start_with?("_")
   outname = "rb_test/sysrb_out/#{basename}.out"
+  puts "testing #{fn}" if verbose
   system("ruby #{fn} > #{outname}")
   unless system("./erruby #{fn} | diff #{outname} -")
     fail_case << fn
