edaffe3b10154251673a8019d73fa71409feba26
johnlinvc <johnlinvc@gmail.com>
2016-05-20 16:02:02 +0800

    add Array#*

--- src/erruby_array.erl
+++ src/erruby_array.erl
@@ -1,4 +1,5 @@
 -module(erruby_array).
+-include("rb.hrl").
 -export([install_array_classes/0, new_array/2, new_array/1]).
 -export([array_to_list/1, push/2]).
 
@@ -8,6 +9,7 @@ install_array_classes() ->
   'Array' = erruby_object:def_global_const('Array', ArrayClass),
   erruby_object:def_method(ArrayClass, map, fun method_map/1),
   erruby_object:def_method(ArrayClass, pmap, fun method_pmap/1),
+  erruby_object:def_method(ArrayClass, '*' , fun method_multiplication/2),
   ok.
 
 method_map(#{self := Self}=Env) ->
@@ -16,6 +18,19 @@ method_map(#{self := Self}=Env) ->
   Envs = erruby_vm:scanl(FoldFun, Env, List),
   Results = lists:map(fun erruby_rb:ret_val/1, Envs),
   erruby_rb:return(Results, lists:last(Envs)).
+
+repeat_list(_List, Count) when Count =< 0 ->
+  new_array([]);
+repeat_list(List, 1) ->
+  List;
+repeat_list(List, Count) ->
+  lists:append(List, repeat_list(List, Count-1)).
+
+method_multiplication(#{self := Self}=Env, IntObj) ->
+  Int = erruby_fixnum:fix_to_int(IntObj),
+  List = array_to_list(Self),
+  ResultList = repeat_list(List, Int),
+  new_array(Env, ResultList).
 
 method_pmap(#{self := Self}=Env) ->
   List = array_to_list(Self),
@@ -26,10 +41,7 @@ method_pmap(#{self := Self}=Env) ->
 
 %TODO maybe use pid to find class
 new_array(Env, Elements) ->
-  ArrayClass = erruby_object:find_global_const('Array'),
-  Properties = #{elements => Elements},
-  {ok, Array} = erruby_object:new_object(ArrayClass, Properties),
-  erruby_rb:return(Array, Env).
+  erruby_rb:return(new_array(Elements), Env).
 
 new_array(Elements) ->
   ArrayClass = erruby_object:find_global_const('Array'),
