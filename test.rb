2be1cf97810013a25db6892ed55f63073594e340
johnlinvc <johnlinvc@gmail.com>
2016-08-14 18:50:49 +0800

    support running test for single file

--- test.rb
+++ test.rb
@@ -1,19 +1,36 @@
 #!/usr/bin/env ruby
 fail_case = []
-verbose = ARGV[0] == "-v"
-Dir.glob("rb_test/*.rb") do |fn|
+verbose = ARGV.include?("-v")
+
+def run_test(fn, verbose:false)
   basename = File.basename(fn,'.rb')
-  next if basename.start_with?("_")
   outname = "rb_test/sysrb_out/#{basename}.out"
   puts "testing #{fn}" if verbose
   system("ruby #{fn} > #{outname}")
-  unless system("./erruby #{fn} | diff #{outname} -")
-    fail_case << fn
-    puts "test #{fn} failed"
+  system("./erruby #{fn} | diff #{outname} -")
+end
+
+if File.exist?(ARGV[0])
+  fn = ARGV[0]
+  basename = File.basename(fn,'.rb')
+  test_result = run_test(fn, verbose: true)
+  fail_case << fn unless test_result
+else
+  Dir.glob("rb_test/*.rb") do |fn|
+    basename = File.basename(fn,'.rb')
+    next if basename.start_with?("_")
+    unless run_test(fn, verbose: verbose)
+      fail_case << fn
+    end
   end
 end
+
 if fail_case.empty?
   puts "everything pass"
+  exit 0
 else
+  fail_case.each do |fn|
+      puts "test #{fn} failed"
+  end
   exit 1
 end
