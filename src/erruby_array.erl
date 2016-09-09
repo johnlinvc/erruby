867a89c87247a01730fc7e5964552bfbed1dba3b
Kenichi Kamiya <kachick1@gmail.com>
2016-09-09 20:05:12 +0900

    Expose Array#push

--- src/erruby_array.erl
+++ src/erruby_array.erl
@@ -13,6 +13,7 @@ install_array_classes() ->
   erruby_object:def_method(ArrayClass, 'at' , fun method_at/2),
   erruby_object:def_method(ArrayClass, 'first' , fun method_first/1),
   erruby_object:def_method(ArrayClass, 'last' , fun method_last/1),
+  erruby_object:def_method(ArrayClass, 'push' , fun method_push/2),
   ok.
 
 method_map(#{self := Self}=Env) ->
@@ -54,6 +55,10 @@ method_first(#{self := Self}=Env) ->
 method_last(#{self := Self}=Env) ->
   List = array_to_list(Self),
   erruby_rb:return(lists:last(List), Env).
+
+method_push(#{self := Self}=Env, Append) ->
+  push(Self, Append),
+  erruby_rb:return(Self, Env).
 
 %TODO maybe use pid to find class
 new_array(Env, Elements) ->
