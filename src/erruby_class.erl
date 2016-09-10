e985ef348707ae0414a8e8fc44f40d1b808e0051
johnlinvc <johnlinvc@gmail.com>
2016-09-10 17:40:27 +0900

    add interface to get class name

--- src/erruby_class.erl
+++ src/erruby_class.erl
@@ -1,12 +1,21 @@
 -module(erruby_class).
--export([new_class/0, new_class/1, install_class_class_methods/0, init_class_class/0]).
+-export([new_class/0, new_named_class/1, new_class/1, install_class_class_methods/0, init_class_class/0, class_name/1]).
 
 %TODO return self when calling method_class on self
 %TODO add name parameter
 %TODO should call initialize method when new
 
 new_class() ->
-  erruby_object:start_link(class_class()).
+  new_named_class(undefined).
+
+new_named_class(Name) ->
+  Properties = #{name => Name},
+  erruby_object:start_link(class_class(), Properties).
+
+class_name(Self) ->
+  Properties = erruby_object:get_properties(Self),
+  #{name := Name} = Properties,
+  Name.
 
 new_class(SuperClass) ->
   Properties = #{superclass => SuperClass},
