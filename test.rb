078f7922864e6cfbfeec0515008376f027c24954
johnlinvc <johnlinvc@gmail.com>
2016-10-14 23:53:32 +0800

    refactor test script

--- test.rb
+++ test.rb
@@ -16,7 +16,7 @@ end.parse!
 
 verbose = options[:verbose]
 
-def run_test(fn, verbose:false)
+def run_single_mri_test(fn, verbose:false)
   basename = File.basename(fn,'.rb')
   outname = "rb_test/sysrb_out/#{basename}.out"
   puts "testing #{fn}" if verbose
@@ -24,21 +24,26 @@ def run_test(fn, verbose:false)
   system("./erruby #{fn} | diff #{outname} -")
 end
 
-fail_case = []
-if ARGV[0] && File.exist?(ARGV[0])
-  fn = ARGV[0]
-  basename = File.basename(fn,'.rb')
-  test_result = run_test(fn, verbose: verbose)
-  fail_case << fn unless test_result
-else
-  Dir.glob("rb_test/*.rb") do |fn|
+def run_mri_tests(verbose:)
+  fail_case = []
+  if ARGV[0] && File.exist?(ARGV[0])
+    fn = ARGV[0]
     basename = File.basename(fn,'.rb')
-    next if basename.start_with?("_")
-    unless run_test(fn, verbose: verbose)
-      fail_case << fn
+    test_result = run_single_mri_test(fn, verbose: verbose)
+    fail_case << fn unless test_result
+  else
+    Dir.glob("rb_test/*.rb") do |fn|
+      basename = File.basename(fn,'.rb')
+      next if basename.start_with?("_")
+      unless run_single_mri_test(fn, verbose: verbose)
+        fail_case << fn
+      end
     end
   end
+  fail_case
 end
+
+fail_case = run_mri_tests(verbose: verbose)
 
 if fail_case.empty?
   puts "everything pass"
