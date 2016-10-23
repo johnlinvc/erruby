db6c81808c4fc0f7569f22f9480a12cd1d2c7f4b
johnlinvc <johnlinvc@gmail.com>
2016-10-23 15:15:45 +0800

    add signature for expane_path

--- src/erruby_lib/erruby_file.erl
+++ src/erruby_lib/erruby_file.erl
@@ -5,4 +5,9 @@
 install_file_classes() ->
   {ok, FileClass} = erruby_class:new_class(),
   'File' = erruby_object:def_global_const('File', FileClass),
+  erruby_object:def_method(FileClass, 'expand_path', fun method_expand_path/3),
   ok.
+
+%FIXME add impl
+method_expand_path(#{self := Self}=Env, Filename, Dirname) ->
+  Env.
